2cac5eea53fb42fefdc26c456789fcb9c29ec005
Sandrita41 <44031406+Sandrita41@users.noreply.github.com>
2021-07-12 23:17:01 +0300

    First homework

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 const h1DOM = document.querySelector("h1");
+const pDOM = document.querySelector("p");
+const aDOM = document.querySelector("a");
+
 console.log(h1DOM);
+console.log(pDOM);
+console.log(aDOM);
+
+//Ar galima į quer'į kelis selektorius sudėti
 h1DOM.innerText = "Labas vakaras!";
-h1DOM.innerHTML = "Labas rytas!";
+pDOM.innerHTML = "Šiandien gražus oras!"
+aDOM.innerHTML = "Spausk mane!"
