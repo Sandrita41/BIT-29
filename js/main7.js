21408f17d9aa9b9f21dac0cccedba82154481b24
Sandrita41 <44031406+Sandrita41@users.noreply.github.com>
2021-07-14 18:51:32 +0300

    7th Unit

---
+++ js/main7.js
@@ -0,0 +1,6 @@
+const minusBtnDOM = document.querySelector(".minus")
+const plusBtnDOM = document.querySelector(".plus");
+const resetBtnDOM = document.querySelector(".reset");
+console.log(minusBtnDOM);
+console.log(plusBtnDOM);
+console.log(resetBtnDOM);
