b998ba9513b05cc7666cc6afe5366321b60da133
Sandrita41 <44031406+Sandrita41@users.noreply.github.com>
2021-07-15 01:30:49 +0300

    3th updates

--- js/main3.js
+++ js/main3.js
@@ -1,17 +1,7 @@
 const h1DOM = document.querySelector("h1");
-const pDOM = document.querySelector("p");
-const aDOM = document.querySelector("a");
-
-console.log(h1DOM);
-console.log(pDOM);
-console.log(aDOM);
-
 //Ar galima į quer'į kelis selektorius sudėti
-h1DOM.innerText = "Labas vakaras!";
 //Kaip konkrečiam h1 uždėti class
 h1DOM.classList.add("simba");
-pDOM.innerHTML = "Šiandien gražus oras!"
-aDOM.innerHTML = "Spausk mane!"
 
 const allIngridientsDOM = document.querySelectorAll("li");
 console.log(allIngridientsDOM);
@@ -19,10 +9,4 @@ for (let i = 0; i < allIngridientsDOM.length; i++) {
     const item = allIngridientsDOM[i];
     console.log(item);
     item.classList.add("darzoves");
-    const itemName = item.innerText;
-    const nameInUppercase = itemName.toUpperCase();
-    console.log(itemName, nameInUppercase);
-    const newArray = [];
-    newArray.push(nameInUppercase);
-    console.log(newArray);
 }
