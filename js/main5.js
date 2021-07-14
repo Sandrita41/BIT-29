f055fc6556595f3a3a223285d1dfa7869efdb147
Sandrita41 <44031406+Sandrita41@users.noreply.github.com>
2021-07-14 17:44:49 +0300

    5th task

--- js/main5.js
+++ js/main5.js
@@ -1,3 +1,18 @@
 const h1DOM = document.querySelector("h1");
+h1DOM.innerHTML = "Zuikio pomidorų daržas";
+h1DOM.classList.add("pavadinimas");
+const allIngridients = document.querySelectorAll("li");
 
-h1DOM.innerHTML = "Zuikio pomidoru darzas";
+for (let i = 0; i < allIngridients.length; i++) {
+    //Susirandame i-taji elementa
+    const item = allIngridients[i];
+    item.classList.add("pomidoras");
+    //Susirandame item value (Name)
+    const itemName = item;
+    //Pakeičiame tekstą
+    itemName.innerText = "Pomidoras";
+    //Žinome su kelintu nariu dirbame
+    console.log(i);
+    console.log(item);
+    console.log(itemName);
+}
