7c17c3151eb0c1655a8c445b19ee584e28b22ead
Sandrita41 <44031406+Sandrita41@users.noreply.github.com>
2021-07-14 18:38:43 +0300

    Without Extra part 6th Unit

--- js/main6.js
+++ js/main6.js
@@ -1,11 +1,15 @@
 const logoDOM = document.querySelector('.logo');
 const hamburgerDOM = document.querySelector('.hamburger');
+const navDOM = document.querySelector("nav");
+console.log(logoDOM);
+console.log(hamburgerDOM);
+console.log(navDOM);
 
 function hamburgerClick() {
-    if (logoDOM.classList.contains('big')) {
-        logoDOM.classList.remove('big');
+    if (logoDOM.classList.contains('big') & navDOM.classList.contains('.visible')) {
+        logoDOM.classList.remove('big') & navDOM.classList.remove('.visible');
     } else {
-        logoDOM.classList.add('big');
+        logoDOM.classList.add('big') & navDOM.classList.add('.visible');
     }
 }
 
