22b513c292b685303db5f04980aa41476f33e1ff
Sandrita41 <44031406+Sandrita41@users.noreply.github.com>
2021-07-14 19:08:28 +0300

    7th Unit function

--- js/main7.js
+++ js/main7.js
@@ -1,6 +1,25 @@
-const minusBtnDOM = document.querySelector(".minus")
+const minusBtnDOM = document.querySelector(".minus");
 const plusBtnDOM = document.querySelector(".plus");
 const resetBtnDOM = document.querySelector(".reset");
+const resultDOM = document.querySelector(".number");
 console.log(minusBtnDOM);
 console.log(plusBtnDOM);
 console.log(resetBtnDOM);
+
+
+function clickButtons() {
+    resultDOM = 0;
+    if (plusBtnDOM.classList.contains('plus')) {
+        //padidinti kintamąjį +1
+        resultDOM = resetBtnDOM + 1;
+    }
+    if (minusBtnDOM.classList.contains('minus')) {
+        resultDOM = resetBtnDOM - 1;
+    }
+    /*if (resetBtnDOM.classList.contains('reset')) {
+        resetBtnDOM.classList.remove;
+    }*/
+}
+
+plusBtnDOM.addEventListener('click', clickButtons);
+minusBtnDOM.addEventListener('click', clickButtons)
